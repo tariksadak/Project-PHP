05d55e4550a7cdfb8079334e57eeb7ed60d3d0be
tarik <100346980+tariksadak@users.noreply.github.com>
2022-08-25 16:47:08 +0100

    Update Database.php

--- Galerie/models/Database.php
+++ Galerie/models/Database.php
@@ -13,7 +13,7 @@ abstract class Database
         
         try
         {
-        self::$_dbCon = new \PDO( 'mysql:host=db.3wa.io;dbname=tariksadak_Canape;charset=utf8', 'tariksadak', '50725e67e31e7b4201b3bc8e2edfdb81' ); //utilisation de pdo pour entrer dans la base de donne
+        self::$_dbCon = new \PDO( '?;?;?', '?', '?' ); //utilisation de pdo pour entrer dans la base de donne
         self::$_dbCon->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING );
         }
         catch (TypeError $e)
